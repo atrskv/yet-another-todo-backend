5cb8a2cc71fc5d6911d4eafb188780e7d18d5152
lrayne <legaterayne@gmail.com>
2025-05-05 20:42:26 +0300

    fix: update config

--- yet_another_todo_backend/config.py
+++ yet_another_todo_backend/config.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-load_dotenv()
+# load_dotenv()
 
 class Settings(BaseSettings):
     data_folder: str = '/tmp/'
-    frontend_url: str = os.getenv('FRONTEND_URL')
+    # frontend_url: str = os.getenv('FRONTEND_URL')
 
 
 settings = Settings()
